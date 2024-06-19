c1062be59d7fe248854383fd18cf48f4668191a9
Mojtaba Imani <sm.imani@gmail.com>
2024-06-19 15:32:26 +0200

    chore: Update Node.js version to 20 in GitHub Actions workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Kuberise',
+  tagline: 'Kubernetes made easy',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -58,9 +58,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Kuberise',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Kuberise Logo',
         src: 'img/logo.svg',
       },
       items: [
